c1757fc0008d5e966a7bd7e954b2b9c58c1ae37a
Aryan Parikh <aryanparikh@Aryans-MacBook-Air.local>
2024-04-22 03:45:11 -0500

    filtering

---
+++ src/components/FilterComponent.js
@@ -0,0 +1,80 @@
+import { useState, useEffect } from 'react';
+import { Select, MenuItem as MuiMenuItem, FormControl, InputLabel, Grid } from '@mui/material';
+import { collection, query, where, getDocs } from 'firebase/firestore';
+import {db} from '../firebase'
+import MenuItem from './MenuItem';
+
+
+
+function FilterComponent() {
+    const [menuItems, setItems] = useState([]);
+    const [filter, setFilter] = useState('');
+
+    async function fetchData() {
+        try {
+            const q = query(collection(db, "locations"));
+            const querySnapshot = await getDocs(q);
+            const fetchedItems = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+            setItems(fetchedItems); 
+        } catch (error) {
+            console.error("Error fetching data: ", error);
+        }
+    }
+
+    async function fetchDataFiltered(filter) {
+        try {
+            const q = query(collection(db, "locations"), where("tags", "array-contains", filter));
+            const querySnapshot = await getDocs(q);
+            const items = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+            setItems(items);
+            console.log(items); 
+        } catch (error) {
+            console.error("Error fetching filtered data: ", error);
+        }
+    }
+
+    useEffect(() => {
+        fetchData(); 
+    }, []);
+    
+    useEffect(() => {
+        if (filter) { 
+            fetchDataFiltered(filter);
+        }
+    }, [filter]);
+
+    const handleChange = (event) => {
+        setFilter(event.target.value);
+        //fetchDataFiltered(event.target.value);
+    };
+
+    return (
+        <div className="dropdowns">
+          <Grid item xs={12}>
+            <FormControl fullWidth>
+              <InputLabel id="location-type-label">Location Type</InputLabel>
+              <Select
+                labelId="location-type-label"
+                id="location-type-select"
+                
+                onChange={handleChange}
+              >
+                <MuiMenuItem value="Nature">Nature</MuiMenuItem>
+                <MuiMenuItem value="Architecture">Architecture</MuiMenuItem>
+                <MuiMenuItem value="Landscape">Landscape</MuiMenuItem>
+                <MuiMenuItem value="Portraits">Portraits</MuiMenuItem>
+                <MuiMenuItem value="City">City</MuiMenuItem>
+                <MuiMenuItem value="Skyline">Skyline</MuiMenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
+
+        <div className="menu-items-container">
+            <div className="menu-items-scrollable">
+                <MenuItem menuItems={menuItems} parent={"location"} />
+            </div>
+        </div>
+      </div>
+    );
+}
+export default FilterComponent;
--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -7,98 +7,10 @@ import SearchBar from './SearchBar';
 import { Select, MenuItem as MuiMenuItem, FormControl, InputLabel, Grid } from '@mui/material';
 import {db} from '../firebase'
 import { collection, doc, setDoc, getDocs } from "firebase/firestore"; 
+import FilterComponent from './FilterComponent';
 
-/*export const menuItems = [
-  {
-    "id": 1,
-    "title": "Zilker Bridge",
-    "description": "Iconic bridge offering scenic views of Austin",
-    "images": ["bridge1.jpg"],
-    "location": "Austin, TX 78746"
-  },
-  {
-    "id": 2,
-    "title": "Ladybird Bridge",
-    "description": "Bridge named after Lady Bird Lake, providing picturesque views",
-    "images": ["bridge2.jpg"],
-    "address" : "222 West Ave, Austin, TX 78701"
-  },
-  {
-    "id": 3,
-    "title": "Texas Capitol",
-    "description": "Historical landmark and seat of Texas government",
-    "images": ["capitol.jpg"]
-  },
-  {
-    "id": 4,
-    "title": "Graffiti Wall",
-    "description": "Colorful mural-covered wall showcasing local art",
-    "images": ["graffiti-wall.jpg"]
-  },
-  {
-    "id": 5,
-    "title": "Austin Skyline",
-    "description": "Panoramic view of downtown Austin's skyline",
-    "images": ["skyline.jpg"]
-  },
-  {
-    "id": 6,
-    "title": "South Congress",
-    "description": "Vibrant street lined with eclectic shops and eateries",
-    "images": ["soco.jpeg"]
-  },
-  {
-    "id": 7,
-    "title": "UT Tower",
-    "description": "Iconic landmark of the University of Texas at Austin",
-    "images": ["tower.jpg"]
-  },
-  {
-    "id": 8,
-    "title": "Under the Bridge",
-    "description": "Famous spot under Congress Avenue Bridge known for bat watching",
-    "images": ["under-bridge.jpg"]
-  },
-  {
-    "id": 9,
-    "title": "Barton Waterfall",
-    "description": "Tranquil waterfall nestled within Barton Creek Greenbelt",
-    "images": ["waterfall.jpg"]
-  },
-  {
-    "id": 10,
-    "title": "Zilker Waterfall",
-    "description": "Scenic waterfall located in Zilker Metropolitan Park",
-    "images": ["zilker-waterfall.jpg"]
-  }
-]
-*/
-
-
-
-const query_res = await getDocs(collection(db, 'locations'));
-  query_res.forEach((doc) => {
-    // doc.data() is never undefined for query doc snapshots
-    console.log(doc.id, " => ", doc.data());
-  });
 
 function HomePage() {
-
-  const [menuItems, setItems] = useState([]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-        try {
-            const querySnapshot = await getDocs(collection(db, "locations"));
-            const itemList = querySnapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
-            setItems(itemList);
-        } catch (error) {
-            console.error("Error fetching data: ", error);
-        }
-    };
-
-    fetchData();
-  }, []);
   
   return (
     <div className="home-page-container">
@@ -108,31 +20,9 @@ function HomePage() {
         <GoogleApiWrapper />
       </div>
 
-      <div className="dropdowns">
-          <Grid item xs={12}>
-            <FormControl fullWidth>
-              <InputLabel id="location-type-label">Location Type</InputLabel>
-              <Select
-                labelId="location-type-label"
-                id="location-type-select"
-                defaultValue=""
-              >
-                <MuiMenuItem value="1">Nature</MuiMenuItem>
-                <MuiMenuItem value="2">Architecture</MuiMenuItem>
-                <MuiMenuItem value="3">Landscape</MuiMenuItem>
-                <MuiMenuItem value="1">Portraits</MuiMenuItem>
-                <MuiMenuItem value="2">City</MuiMenuItem>
-                <MuiMenuItem value="3">Skyline</MuiMenuItem>
-              </Select>
-            </FormControl>
-          </Grid>
-      </div>
+      <FilterComponent component />
 
-      <div className="menu-items-container">
-        <div className="menu-items-scrollable">
-          <MenuItem menuItems={menuItems} parent={"location"} />
-        </div>
-      </div>
+      
     </div>
   );
 }
