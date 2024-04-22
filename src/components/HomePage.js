27b74b3da1321d550f2b2c2874627ec0d6f683b3
sourav <sourav@utexas.edu>
2024-04-22 16:52:51 -0500

    functioning markers

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -8,21 +8,24 @@ import { Select, MenuItem as MuiMenuItem, FormControl, InputLabel, Grid } from '
 import {db} from '../firebase'
 import { collection, doc, setDoc, getDocs } from "firebase/firestore"; 
 import FilterComponent from './FilterComponent';
+import MapWithSearch from './MapWithSearch';
+import { useNavigate } from "react-router-dom";
 
 
 function HomePage() {
-  
+  const navigate = useNavigate();
+  const handleNavigation = (location) => {
+    navigate(location);
+  };
+
   return (
     <div className="home-page-container">
       <SearchBar/>
       <div className='spacer'/>
       <div className="map-container">
-        <GoogleApiWrapper />
+        <MapWithSearch handleNavigation={handleNavigation}/>
       </div>
-
       <FilterComponent component />
-
-      
     </div>
   );
 }
--- src/components/MapWithSearch.js
+++ src/components/MapWithSearch.js
@@ -1,96 +1,17 @@
-// import React, { Component } from 'react';
-// import { Map, GoogleApiWrapper } from 'google-maps-react';
-// import "./map.css";
-
-// class MapWithSearch extends Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       userLocation: null,
-//       mapCenter: { lat: 37.774929, lng: -122.419416 }, // Default to San Francisco coordinates
-//       showUserLocation: true, // Enable user location
-//       mapLoaded: false // Flag to indicate whether the map has loaded
-//     };
-//   }
-
-//   componentDidMount() {
-//     if (navigator.geolocation) {
-//       navigator.geolocation.getCurrentPosition(
-//         (position) => {
-//           const userLocation = {
-//             lat: position.coords.latitude,
-//             lng: position.coords.longitude
-//           };
-//           this.setState({
-//             userLocation,
-//             mapCenter: userLocation, // Set mapCenter to user's location once available
-//             mapLoaded: true // Mark the map as loaded
-//           });
-//         },
-//         (error) => {
-//           console.error('Error getting user location:', error);
-//           this.setState({ mapLoaded: true }); // Mark the map as loaded even if there's an error
-//         }
-//       );
-//     } else {
-//       console.error('Geolocation is not supported by this browser.');
-//       this.setState({ mapLoaded: true }); // Mark the map as loaded if geolocation is not supported
-//     }
-//   }
-
-//   render() {
-//     const { mapLoaded, mapCenter } = this.state;
-
-//     const mapContainerStyle = {
-//       width: '100%', // Set the width to 100%
-//       height: '400px',
-//       position: 'relative',
-//       display: 'flex',
-//       justifyContent: 'center',
-//       alignItems: 'center',
-//     };
-
-//     const mapStyles = {
-//       width: '100%', // Set map width to 100% within its container
-//       height: '100%', // Set map height to 100% within its container
-//     };
-
-//     return (
-//       <div className="search-bar" style={mapContainerStyle}>
-//         {!mapLoaded && <div>Loading...</div>}
-//         {mapLoaded && 
-//         <Map
-//           google={this.props.google}
-//           zoom={14}
-//           style={mapStyles}
-//           initialCenter={mapCenter} // Set initialCenter to mapCenter state value
-//           streetViewControl={false}
-//           showUserLocation={true}
-//         >
-
-//         </Map>
-//         }
-//       </div>
-//     );
-//   }
-// }
-
-// export default GoogleApiWrapper({
-//   apiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-// })(MapWithSearch);
-
-import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
-import "./map.css";
+import React, { Component } from "react";
+import { Map, GoogleApiWrapper, Marker } from "google-maps-react";
+import { db } from "../firebase";
+import { collection, query, where, getDocs } from "firebase/firestore";
 
 class MapWithSearch extends Component {
   constructor(props) {
     super(props);
     this.state = {
       userLocation: null,
-      mapCenter: { lat: 37.774929, lng: -122.419416 }, // Default to San Francisco coordinates
-      showUserLocation: true, // Enable user location
-      mapLoaded: false // Flag to indicate whether the map has loaded
+      mapCenter: { lat: 37.774929, lng: -122.419416 },
+      showUserLocation: true,
+      mapLoaded: false,
+      menuItems: [], // Add state for menu items
     };
   }
 
@@ -100,6 +21,21 @@ class MapWithSearch extends Component {
       this.geocodeAddress(location);
     } else {
       this.getCurrentLocation();
+      this.fetchData();
+    }
+  }
+
+  async fetchData() {
+    try {
+      const q = query(collection(db, "locations"));
+      const querySnapshot = await getDocs(q);
+      const fetchedItems = querySnapshot.docs.map((doc) => ({
+        id: doc.id,
+        ...doc.data(),
+      }));
+      this.setState({ menuItems: fetchedItems }); // Update menuItems state
+    } catch (error) {
+      console.error("Error fetching data: ", error);
     }
   }
 
@@ -109,78 +45,104 @@ class MapWithSearch extends Component {
         (position) => {
           const userLocation = {
             lat: position.coords.latitude,
-            lng: position.coords.longitude
+            lng: position.coords.longitude,
           };
           this.setState({
             userLocation,
             mapCenter: userLocation, // Set mapCenter to user's location once available
-            mapLoaded: true // Mark the map as loaded
+            mapLoaded: true, // Mark the map as loaded
           });
         },
         (error) => {
-          console.error('Error getting user location:', error);
+          console.error("Error getting user location:", error);
           this.setState({ mapLoaded: true }); // Mark the map as loaded even if there's an error
         }
       );
     } else {
-      console.error('Geolocation is not supported by this browser.');
+      console.error("Geolocation is not supported by this browser.");
       this.setState({ mapLoaded: true }); // Mark the map as loaded if geolocation is not supported
     }
   }
 
   geocodeAddress(address) {
+    // Implement geocodeAddress function
     const { google } = this.props;
     const geocoder = new google.maps.Geocoder();
     geocoder.geocode({ address }, (results, status) => {
-      if (status === 'OK' && results[0]) {
+      if (status === "OK" && results[0]) {
         const location = results[0].geometry.location;
         const mapCenter = {
           lat: location.lat(),
-          lng: location.lng()
+          lng: location.lng(),
         };
         this.setState({
           mapCenter,
-          mapLoaded: true
+          mapLoaded: true,
         });
       } else {
-        console.error('Geocode was not successful for the following reason:', status);
+        console.error(
+          "Geocode was not successful for the following reason:",
+          status
+        );
         this.getCurrentLocation();
       }
     });
   }
 
+  handleMarkerClick(markerData) {
+    const { Name } = markerData;
+    // Call the handleNavigation function passed from the parent component
+    this.props.handleNavigation(`/location/${Name}`);
+  }
+
   render() {
-    const { mapLoaded, mapCenter } = this.state;
+    const { mapCenter, mapLoaded, menuItems } = this.state;
 
     const mapContainerStyle = {
-      width: '100%', // Set the width to 100%
-      height: '400px',
-      position: 'relative',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
+      width: "100%",
+      height: "400px",
+      position: "relative",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
     };
 
     const mapStyles = {
-      width: '100%', // Set map width to 100% within its container
-      height: '100%', // Set map height to 100% within its container
+      width: "100%",
+      height: "100%",
     };
 
     return (
       <div className="search-bar" style={mapContainerStyle}>
         {!mapLoaded && <div>Loading...</div>}
-        {mapLoaded && 
-        <Map
-          google={this.props.google}
-          zoom={14}
-          style={mapStyles}
-          initialCenter={mapCenter} // Set initialCenter to mapCenter state value
-          streetViewControl={false}
-          showUserLocation={true}
-        >
-
-        </Map>
-        }
+        {mapLoaded && (
+          <Map
+            google={this.props.google}
+            zoom={14}
+            style={mapStyles}
+            initialCenter={mapCenter}
+            streetViewControl={false}
+            showUserLocation={true}
+          >
+            {menuItems.map((item) => (
+              <Marker
+                key={item.id}
+                position={{
+                  lat: item.coords.latitude,
+                  lng: item.coords.longitude,
+                }}
+                title={item.Name}
+                onClick={() => this.handleMarkerClick(item)}
+              />
+            ))}
+            <Marker
+              position={{ lat: mapCenter.lat, lng: mapCenter.lng }}
+              icon={{
+                url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png", // Blue marker icon
+              }}
+            />
+          </Map>
+        )}
       </div>
     );
   }
@@ -189,4 +151,3 @@ class MapWithSearch extends Component {
 export default GoogleApiWrapper({
   apiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
 })(MapWithSearch);
-
